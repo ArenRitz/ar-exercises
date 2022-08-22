1adf7d126257a1632b61453b0ce7efbc7baff3a8
ArenRitz <aren.dolbakian@gmail.com>
2022-08-22 19:18:38 -0400

    Complete exercise 6, add employees and relation in models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 50)
+@store1.employees.create(first_name: "Ritz", last_name: "Spaghetti", hourly_rate: 130)
+@store2.employees.create(first_name: "Annie", last_name: "Pasta", hourly_rate: 45)
+@store2.employees.create(first_name: "Ryatt", last_name: "Now", hourly_rate: 59)
+@store1.employees.create(first_name: "Davion", last_name: "Knight", hourly_rate: 100)
+@store1.employees.create(first_name: "Invo", last_name: "Ker", hourly_rate: 2)
